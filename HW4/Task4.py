e6fbbfdafe530c3ff7cd55fad39ffc0d9f245810
annmark94 <annmarkova94@gmail.com>
2022-10-15 17:44:08 +0300

    Added HW4 Task4.py v2

--- HW4/Task4.py
+++ HW4/Task4.py
@@ -6,17 +6,34 @@ import random
 
 def func_generator(k):
     func = ""
-    for i in range(0, k):
-        koeff = random.randint(0, 11)
+    a = 0
+    for i in range(k, -1, -1):
+        koeff = random.randint(-10, 11)
         # print(f'{i}, koeff = {koeff}')
-        if koeff != 0:
-            if i == 0:
-                func = f'{koeff}' + func
+        if koeff > 0:
+            if i == 0 and a == 0:
+                func += f'{koeff}'
+            elif i == 0:
+                func += f' + {koeff}'
+            elif a == 0:
+                func += f'{koeff}*x^{i}'
             else:
-                func = f'{koeff}*x^{i} + ' + func  
+                func += f' + {koeff}*x^{i}'
+            a += 1
+        if koeff < 0:
+            if i == 0 and a == 0:
+                func += f' - {-koeff}'
+            elif i == 0:
+                func += f' - {-koeff}'
+            elif a == 0:
+                func += f' - {-koeff}*x^{i}'
+            else:
+                func += f' - {-koeff}*x^{i}'
+            a += 1
     return func
 
 k = int(input('Enter an integer: '))
+# print(func_generator(k))
 
 with open(r'C:\Users\annmark\YandexDisk\GB\8. Python\GB_Python_Homeworks\HW4\HW4_Task4.txt', 'a') as data:
     data.write(func_generator(k)+'\n')
